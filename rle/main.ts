d4f4bf31d80def747157cfd7538f68963b056007
Philipp Plotnikov <philipp.plotnikov@icloud.com>
2024-03-10 19:39:46 +0500

    feat: add read/write file for rle

--- rle/main.ts
+++ rle/main.ts
@@ -1,13 +1,39 @@
+import { Modes } from "./types";
 import { decode, encode } from "./utils/utils";
+import * as fs from "fs";
 
-const data = "#".repeat(280);
-console.log("Original data: ", data);
-console.log("Original data length: ", data.length);
-console.log("-----------");
-const encodedData = encode(data);
-console.log("Encoded data: ", encodedData);
-console.log("Encoded data length: ", encodedData.length);
-console.log("-----------");
-const decodedData = decode(encodedData);
-console.log("Decoded data: ", decodedData);
-console.log("Decoded data length: ", decodedData.length);
+try {
+    const [mode, inputFilename, outputFilename] = process.argv.slice(2);
+    if (!fs.existsSync(inputFilename)) {
+        throw new Error(`File "${inputFilename}" is not exist`);
+    }
+    const data = fs.readFileSync(inputFilename).toString();
+    switch (mode) {
+        case Modes.ENCODE:
+            const encodedData = encode(data);
+            fs.writeFileSync(outputFilename, encodedData);
+            break;
+        case Modes.DECODE:
+            const decodedData = decode(data);
+            fs.writeFileSync(outputFilename, decodedData);
+            break;
+        default:
+            throw new Error(`Mode paramater value should be "${Modes.ENCODE}" or "${Modes.DECODE}"`)
+    }
+} catch (error) {
+    console.error(error);
+}
+
+
+
+// const data = "#".repeat(280);
+// console.log("Original data: ", data);
+// console.log("Original data length: ", data.length);
+// console.log("-----------");
+// const encodedData = encode(data);
+// console.log("Encoded data: ", encodedData);
+// console.log("Encoded data length: ", encodedData.length);
+// console.log("-----------");
+// const decodedData = decode(encodedData);
+// console.log("Decoded data: ", decodedData);
+// console.log("Decoded data length: ", decodedData.length);
