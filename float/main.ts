7b55802d2548eaa974b2289420dc9a57020f5b91
Philipp Plotnikov <philipp.plotnikov@icloud.com>
2024-04-14 18:43:28 +0500

    refactor: remove unused import from float

--- float/main.ts
+++ float/main.ts
@@ -1,6 +1,6 @@
-import { convertToFloat, decodeFloat } from "./utils/utils";
+import { decodeFloat } from "./utils/utils";
 
-const data = 0.6666666;
+const data = 0.33333333333;
 try {
     console.log("Original number: ", data);
     console.log("------------");
@@ -12,7 +12,8 @@ try {
     // console.log("Int view: ", int);
     // console.log("Length: ", int.length);
     // console.log("------------");
-    const binaryFloat = convertToFloat(data);
+    // const binaryFloat = convertToFloat(data);
+    const binaryFloat = "0x3eaaaaaa";
     console.log("Binary float view: ", binaryFloat);
     const float = decodeFloat(binaryFloat);
     console.log("------------");
