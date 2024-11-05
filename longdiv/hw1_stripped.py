19a60b5f0a699e77bead8ec95ce592c9b9521325
Philipp Plotnikov <philipp.plotnikov@icloud.com>
2024-11-05 20:59:12 +0500

    refactor: refaactor longdiv

--- longdiv/hw1_stripped.py
+++ longdiv/hw1_stripped.py
@@ -1,37 +1,10 @@
 #!/usr/bin/env python3
 
 def long_division(dividend, divider):
-    '''
-    Вернуть строку с процедурой деления «уголком» чисел dividend и divider.
-    Формат вывода приведён на примерах ниже.
-
-    Примеры:
-    >>> 12345÷25
-    12345|25
-    100  |493
-     234
-     225
-       95
-       75
-       20
-
-    >>> 1234÷1423
-    1234|1423
-    1234|0
-
-    >>> 24600÷123
-    24600|123
-    246  |200
-      0
-
-    >>> 246001÷123
-    246001|123
-    246   |2000
-         1
-    '''
     result, steps = get_division_result_and_steps(dividend, divider)
     formatted_result = get_division_format_output(
-        dividend, divider, result, steps)
+        dividend, divider, result, steps
+    )
     return formatted_result
 
 
@@ -46,7 +19,9 @@ def get_division_result_and_steps(dividend, divider):
         remainder = current % divider
         result += str(quotient_digit)
         if quotient_digit != 0 or index == len(dividend_str) - 1:
-            steps.append((current, quotient_digit * divider, remainder))
+            steps.append(
+                (current, quotient_digit * divider, remainder)
+            )
     return (result, steps)
 
 
@@ -60,11 +35,13 @@ def get_division_format_output(dividend, divider, result, steps):
         diff = current - product
         if index == 0:
             if product == 0:
-                formatted_result += f"{current}|{result.lstrip("0") or "0"}\n"
+                cleared_result = result.lstrip('0') or '0'
+                formatted_result += f"{current}|{cleared_result}\n"
                 is_print_after_loop = False
             else:
-                formatted_result += f"{product}{' ' * (len(dividend_str) - len(str(product)))}|{ \
-                    result.lstrip("0") or "0"}\n"
+                indents = ' ' * (len(dividend_str) - len(str(product)))
+                cleared_result = result.lstrip('0') or '0'
+                formatted_result += f"{product}{indents}|{cleared_result}\n"
             if diff != 0:
                 position += len(str(current)) - len(str(diff))
             else:
@@ -73,24 +50,22 @@ def get_division_format_output(dividend, divider, result, steps):
         if current == 0:
             continue
         if product == 0:
-            formatted_result += f"{' ' * \
-                                   (len(dividend_str) - len(str(current)))}{current}\n"
+            indents = ' ' * (len(dividend_str) - len(str(current)))
+            formatted_result += f"{indents}{current}\n"
             is_print_after_loop = False
         else:
             formatted_result += f"{' ' * position}{current}\n"
-            formatted_result += f"{' ' * \
-                                   (position + len(str(current)) - len(str(product)))}{product}\n"
+            indents = ' ' * (position + len(str(current)) - len(str(product)))
+            formatted_result += f"{indents}{product}\n"
         if diff != 0:
             position += len(str(current)) - len(str(diff))
         else:
             position += len(str(current)) - 1 or 1
-
     if is_print_after_loop:
         if position == len(dividend_str):
             position -= 1
         formatted_result += f"{' ' * position}{remainder}\n"
-
-    return formatted_result
+    return formatted_result[0:-1]
 
 
 def main():
